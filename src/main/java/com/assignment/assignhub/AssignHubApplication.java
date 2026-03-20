8afe4e769c23a095f51e8c848de79c8a11722c72
Felix Kebaki <felixkebaki@gmail.com>
2026-03-20 13:11:18 +0300

    Initial commit

---
+++ src/main/java/com/assignment/assignhub/AssignHubApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.assignhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assignment/assignhub/AssignHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.assignhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
