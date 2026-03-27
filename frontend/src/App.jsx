917b336108527fb3021807f4d052eab4d336e65a
Felix Kebaki <felixkebaki@gmail.com>
2026-03-27 14:58:44 +0300

    Frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom"
+import { LayoutWithNavbar } from "./components/Layouts/LayoutWithNavbar"
+import { LayoutWithoutNavbar } from "./components/Layouts/LayoutWithoutNavbar"
+import { LoginPage } from "./pages/LoginPage"
+import { RegisterPage } from "./pages/RegisterPage"
+import { HomePage } from "./pages/HomePage"
 
 function App() {
 
   return (
     <>
-      Hello
+      <Router>
+        <Routes>
+
+          <Route element={<LayoutWithNavbar/>}>
+              <Route path="/" element={<HomePage/>}/>
+          </Route>
+
+          <Route element={<LayoutWithoutNavbar/>}>
+              <Route path="/login" element={<LoginPage/>}/>
+              <Route path="/register" element={<RegisterPage/>}/>
+          </Route>
+
+        </Routes>
+      </Router>
     </>
   )
 }
